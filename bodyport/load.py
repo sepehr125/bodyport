a5664d1a78ee93abf26ee752b52ab911146ec3a8
Sepehr <sepehr125@gmail.com>
2020-03-31 18:33:45 -0700

    add hash checksum check

--- bodyport/load.py
+++ bodyport/load.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 from datetime import datetime, date
 from pathlib import Path
@@ -6,7 +7,6 @@ from typing import List, Dict
 import pandas as pd
 
 from bodyport.orm import Base, Subject, Run, create_session
-from bodyport.config import DB_PATH
 
 
 class DataWarehouseManager:
@@ -16,59 +16,71 @@ class DataWarehouseManager:
 
     """
 
-    def __init__(self):
-        # recreates database
+    def __init__(self, db_conn_string=None):
         self.data_dir = None
         self.current_time = None
-        self.db_session = create_session()
 
-    @property
-    def status(self):
-        return 'up' if DB_PATH.is_file() else 'down'
+        self.db_session = create_session(db_conn_string=db_conn_string)
+
+        # get path to database
+        self.sqlite_path = Path(self.db_session.bind.url.database)
+
+    ###################
+    # DB ADMIN METHODS
+    ###################
 
     def down(self):
         # using sqlite, so just delete the file
-        DB_PATH.unlink(missing_ok=True)
+        self.sqlite_path.unlink(missing_ok=True)
 
     def up(self):
         engine = self.db_session.bind
         Base.metadata.create_all(engine)
 
-    def update(self, data_dir: Path):
+    def empty(self):
+        self.down()
+        self.up()
+
+    def load(self, data_dir: Path):
         """
-        Main method
+        process the given data directory,
+        creating records for runs and their corresponding subjects.
+
+        NB: Aside from the subject number in the path, there isn't anything
+        else about the subject except what is in run metadata.
+
+        So we populate all run metadata first, then update the subject table if needed.
+
+        This way we can be sure to also catch if a subject_id is reused but the age/sex etc dramatically
         """
+        # we'll timestamp all new records with the same time timestamp
+        current_time = datetime.now()
+
         self.data_dir = data_dir
-        self.current_time = datetime.now()
 
-        self.update_runs()
-        self.update_subjects()
+        self.update_runs(timestamp=current_time)
+        self.update_subjects(timestamp=current_time)
 
-    def find_run_paths(self) -> List[Path]:
+    ##############
+    # DB reconciliation and inserts
+    ##############
+    def update_runs(self, timestamp: datetime):
         """
-        Fetch all CSV paths.
+
+        :param timestamp:
+        :return:
         """
-        return self.data_dir.glob('*/run_*.csv')
-
-    def run_exists_in_db(self, subject_id: int, run_number: int) -> bool:
-        result = self.db_session.query(Run).filter_by(
-            subject_id=subject_id,
-            number=run_number
-        )
-        return result.count() > 0
-
-    def update_runs(self):
         for run_path in self.find_run_paths():
-
             maybe_new_run = self.generate_run_from_path(run_path)
 
-            if not self.run_exists_in_db(subject_id=maybe_new_run.subject_id, run_number=maybe_new_run.number):
-                maybe_new_run.processed_at = self.current_time
+            if not self.run_exists_in_db(maybe_new_run):
+                maybe_new_run.created_at = timestamp
                 self.db_session.add(maybe_new_run)
                 self.db_session.commit()
 
-    def update_subjects(self):
+    def update_subjects(self, timestamp: datetime):
 
+        # crawl subject_id's for all runs
         maybe_new_subjects = self.db_session.query(
             Run.subject_id,
             Run.age_at_run,
@@ -79,10 +91,62 @@ class DataWarehouseManager:
         for subject in maybe_new_subjects:
 
             if not self.subject_exists_in_db(subject_id=subject.subject_id):
-                birth_year = subject.date.year - subject.age_at_run
-                new_subject = Subject(id=subject.subject_id, sex=subject.sex, birth_year=birth_year)
+                new_subject = Subject(
+                    id=subject.subject_id,
+                    sex=subject.sex,
+                    birth_year=subject.date.year - subject.age_at_run,
+                    created_at=timestamp
+                )
                 self.db_session.add(new_subject)
                 self.db_session.commit()
+
+    ##############################
+    # Filesystem Crawler Methods
+    ##############################
+    def find_run_paths(self) -> List[Path]:
+        """
+        Fetch all CSV paths in self.data_dir
+
+        NB: this is really a "crawler" that should be refactored into its own class
+        """
+        return self.data_dir.glob('*/run_*.csv')
+
+    @classmethod
+    def get_run_csv_path(cls, run_path: Path) -> Path:
+        """Whether the original path was CSV or JSON, this will return the CSV path"""
+        file_name = f"run_{cls.parse_run_number(run_path)}.csv"
+        return run_path.with_name(file_name)
+
+    @classmethod
+    def get_run_json_path(cls, run_path: Path) -> Path:
+        """Whether the given path was CSV or JSON, this will return the JSON path"""
+        file_name = f"run_{cls.parse_run_number(run_path)}_header.json"
+        return run_path.with_name(file_name)
+
+    #################################
+    # DB lookup methods
+    #################################
+
+    def pandas_query(self, query):
+        return pd.read_sql(query, con=self.db_session.bind)
+
+    def run_exists_in_db(self, run: Run) -> bool:
+        """
+        How do we identify a unique run?
+
+        We can consider subject_id x run_numbers to be a unique combination, but there is no guarantee
+        that the clinic will keep track of run_numbers, or even subject numbers for that matter.
+
+        The safest way is to create a "signature" of the raw data itself and check new files
+        against it. The data sizes are small enough right now that this is a kind of safety
+        we can afford to ensure data integrity. We may have to compromise later with different
+        "identity resolution" techniques that scale better to large datasets.
+
+        :param run:
+        :return:
+        """
+        result = self.db_session.query(Run).filter_by(subject_id=run.subject_id, run_hash=run.run_hash)
+        return result.count() > 0
 
     def subject_exists_in_db(self, subject_id: int):
         result = self.db_session.query(Subject).filter_by(id=subject_id)
@@ -112,24 +176,13 @@ class DataWarehouseManager:
             units=meta['units'],
             fs=meta['fs'],
             age_at_run=meta['age'],
-            sex=meta['sex']
+            sex=meta['sex'],
+            run_hash=cls.generate_hash_from_raw(run_path)
         )
 
     @classmethod
-    def get_run_csv_path(cls, run_path: Path) -> Path:
-        """Whether the original path was CSV or JSON, this will return the CSV path"""
-        file_name = f"run_{cls.parse_run_number(run_path)}.csv"
-        return run_path.with_name(file_name)
-
-    @classmethod
-    def get_run_json_path(cls, run_path: Path) -> Path:
-        """Whether the original path was CSV or JSON, this will return the JSON path"""
-        file_name = f"run_{cls.parse_run_number(run_path)}_header.json"
-        return run_path.with_name(file_name)
-
-    @classmethod
     def parse_date(cls, run_meta: Dict) -> date:
-        return datetime.strptime(run_meta['date'],  '%d.%m.%Y').date()
+        return datetime.strptime(run_meta['date'], '%d.%m.%Y').date()
 
     @classmethod
     def parse_birth_year(cls, run_meta: Dict) -> int:
@@ -158,6 +211,8 @@ class DataWarehouseManager:
     def parse_subject_id(run_path: Path) -> int:
         return int(run_path.parent.stem.lstrip('subject_'))
 
-    def pandas_query(self, query):
-        return pd.read_sql(query, con=self.db_session.bind)
-
+    @staticmethod
+    def generate_hash_from_raw(run_path: Path) -> str:
+        with open(run_path.as_posix(), 'r') as f:
+            contents = f.read().encode('utf-8')
+        return hashlib.md5(contents).hexdigest()
--- bodyport/orm.py
+++ bodyport/orm.py
@@ -2,27 +2,25 @@ import json
 from typing import Dict
 
 import pandas as pd
-from sqlalchemy import Column, Integer, String, Date, DateTime
+from sqlalchemy import Column, Integer, String, Date, DateTime, Float
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 
 from bodyport.config import DB_CONN_STRING
 
+
+def get_engine(db_conn_string):
+    return create_engine(db_conn_string)
+
+
+def create_session(db_conn_string=None) -> Session:
+    # default to global conn string if none is passed
+    conn_str = db_conn_string or DB_CONN_STRING
+    return Session(bind=get_engine(conn_str))
+
+
 Base = declarative_base()
-
-
-def get_engine():
-    return create_engine(DB_CONN_STRING)
-
-
-def create_session() -> Session:
-    return Session(bind=get_engine())
-
-
-##############
-
-
 
 
 class Subject(Base):
@@ -32,6 +30,8 @@ class Subject(Base):
 
     __tablename__ = 'subject'
 
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
     id = Column(Integer, primary_key=True)
     sex = Column(String)
     birth_year = Column(Integer)
@@ -51,8 +51,10 @@ class Run(Base):
     # the run number isn't really an identifier of the run, because different subjects have the same run
     # the correct primary_key here would be a "subject_number-run_number". But for now we won't formalize
     # this constraint due to the fact that this is a toy example.
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
     id = Column(Integer, primary_key=True)
-    subject_id = Column(Integer)
+    subject_id = Column(Integer)   # this should technically be a foreign key
     number = Column(Integer)
     clinic_id = Column(String)
     measurement = Column(String)
@@ -61,11 +63,10 @@ class Run(Base):
     fs = Column(Integer)
     raw_path = Column(String)
     meta_path = Column(String)
-    processed_path = Column(String)
-    processed_at = Column(DateTime)
     age_at_run = Column(Integer)
     sex = Column(String)
     run_hash = Column(String)
+    avg_bpm = Column(Float)
 
     def __repr__(self):
         return f"Run<subject_id={self.subject_id}, number: {self.number}, date: {self.date}>"
@@ -78,4 +79,3 @@ class Run(Base):
     @property
     def raw(self) -> pd.DataFrame:
         return pd.read_csv(self.raw_path)
-
